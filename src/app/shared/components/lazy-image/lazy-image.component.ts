b74c908f2ea2e8d65d35fab0d132bf2ac64082ab
lumusitech <lumusitech@gmail.com>
2023-04-23 18:24:10 -0300

    feat: lazy load image component added in shared module

--- src/app/shared/components/lazy-image/lazy-image.component.ts
+++ src/app/shared/components/lazy-image/lazy-image.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'shared-lazy-image',
   templateUrl: './lazy-image.component.html',
 })
-export class LazyImageComponent {}
+export class LazyImageComponent implements OnInit {
+  @Input()
+  public url!: string;
+
+  @Input()
+  public alt!: string;
+
+  public hasLoaded: boolean = false;
+
+  ngOnInit(): void {
+    if (!this.url) throw new Error('url property is required ');
+    if (!this.alt) throw new Error('alt property is required ');
+  }
+
+  onLoad() {
+    this.hasLoaded = true;
+  }
+}
